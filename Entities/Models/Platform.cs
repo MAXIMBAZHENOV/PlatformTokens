57541599017cfe3e66282a888f873bb2800ee584
bazhenov-2001 <bazhenov-2001@mail.ru>
2022-04-09 10:42:12 +0300

    Input information for Platform

---
+++ Entities/Models/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    class Platform
+    {
+        [Column("PlatformID")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Platform name is a required field.")]
+        [MaxLength(60,ErrorMessage ="Maximum length for the Name is 60 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Type of consensus is a required field")]
+        public string ConsensusAlgorithm { get; set; }
+
+        public ICollection<Token> Tokens { get; set; }
+
+        
+    }
+}
